568e0824042155a88c42cf5a9d6670b16920ef64
hdddhdd <nuly08@naver.com>
2022-09-23 02:10:25 +0900

    header and footer

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index,login,example
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', login),
+    path('ex/', example),
+
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'core/index.html')
+
+
+def login(request):
+	return render(request, 'core/login.html')
+
+#예제 코드
+def example(request):
+	return render(request, 'core/example.html')
