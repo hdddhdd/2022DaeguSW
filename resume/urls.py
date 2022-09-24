0d5cd59370e30696462f9459339f2dfc3face82b
hdddhdd <nuly08@naver.com>
2022-09-25 02:38:59 +0900

    navbar 정리

--- resume/urls.py
+++ resume/urls.py
@@ -1,12 +1,14 @@
 
 from django.urls import path
-from .views import resume
+from .views import resume, mypage,myresume
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('resume/', resume, name='resume'),
-   
+    path('mypage/', mypage, name='mypage'),
+    path('myresume', myresume,name='myresume'),
+
 ]
 
 if settings.DEBUG: 
